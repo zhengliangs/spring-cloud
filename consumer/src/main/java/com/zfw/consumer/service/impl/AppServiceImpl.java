7c670ac99eb49d4e889ca7ffd56e27f9d79a0bb6
zhengliangs <zhengliang166@126.com>
2019-09-03 17:09:17 +0800

    提交

---
+++ consumer/src/main/java/com/zfw/consumer/service/impl/AppServiceImpl.java
@@ -0,0 +1,11 @@
+package com.zfw.consumer.service.impl;
+
+import com.zfw.consumer.service.AppService;
+
+public class AppServiceImpl implements AppService {
+
+    @Override
+    public String getName(Integer id) {
+        return null;
+    }
+}
