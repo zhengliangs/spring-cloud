7956d549c2c08b501be74fea210084890f256580
zhengll <zhengliang166@126.com>
2019-09-07 19:26:16 +0800

    测试读取配置文件的值

--- provider/src/main/java/com/zfw/provider/conteoller/UserController.java
+++ provider/src/main/java/com/zfw/provider/conteoller/UserController.java
@@ -1,5 +1,6 @@
 package com.zfw.provider.conteoller;
 
+import com.zfw.provider.entity.PropertiesEntity;
 import com.zfw.provider.entity.UserEntity;
 import com.zfw.provider.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class UserController {
     @Autowired
     public UserService userService;
 
+    @Autowired
+    private PropertiesEntity propertiesEntity;
+
     @GetMapping(value = "/getUserInfo")
     public UserEntity getUserInfo(){
         return userService.getUserInfo();
@@ -21,7 +25,11 @@ public class UserController {
 
     @GetMapping(value = "getName")
     public String getName(Integer id){
-        System.out.println("id==="+id);
         return "张三";
+    }
+
+    @GetMapping(value = "/get")
+    public PropertiesEntity get() {
+        return propertiesEntity;
     }
 }
