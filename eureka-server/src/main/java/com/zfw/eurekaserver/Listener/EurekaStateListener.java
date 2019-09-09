06d44ce805ba4c0d3288713282df9bee5bd5920a
zhengliangs <zhengliang166@126.com>
2019-09-09 17:51:31 +0800

    服务监听

--- eureka-server/src/main/java/com/zfw/eurekaserver/Listener/EurekaStateListener.java
+++ eureka-server/src/main/java/com/zfw/eurekaserver/Listener/EurekaStateListener.java
@@ -1,38 +1,42 @@
 package com.zfw.eurekaserver.Listener;
 
 import com.netflix.appinfo.InstanceInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.netflix.eureka.server.event.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
+@Slf4j
 public class EurekaStateListener {
 
     @EventListener
     public void listen(EurekaInstanceRegisteredEvent event) {
         InstanceInfo instanceInfo = event.getInstanceInfo();
-        System.out.println("服务{}进行注册"+instanceInfo.getAppName()+ instanceInfo.getHostName() +"  "+ instanceInfo.getIPAddr() +"  "+ instanceInfo.getPort());
+        log.info("****服务进行注册****", instanceInfo.getAppName()+ instanceInfo.getHostName() +"  "+ instanceInfo.getIPAddr() +"  "+ instanceInfo.getPort());
     }
 
     @EventListener
     public void listen(EurekaInstanceCanceledEvent event) {
-        System.out.println("服务{}已下线"+event.getAppName());
-        System.out.println("server地址信息{}"+event.getServerId());
+        log.info("****服务已下线****", event.getAppName(), event.getServerId());
     }
 
     @EventListener
     public void listen(EurekaInstanceRenewedEvent event) {
-        System.out.println( "服务{}进行续约"+event.getServerId() +"  "+ event.getAppName());
+        LocalDateTime now = LocalDateTime.now();
+        log.info( "****服务进行续约****"+now.getHour()+":"+now.getMinute()+":"+now.getSecond()+event.getAppName()+event.getServerId());
     }
 
     @EventListener
     public void listen(EurekaRegistryAvailableEvent event) {
-//        logger.info("注册中心启动,{}", System.currentTimeMillis());
+        log.info("****注册中心启动****", System.currentTimeMillis());
     }
 
     @EventListener
     public void listen(EurekaServerStartedEvent event) {
-//        logger.info("注册中心服务端启动,{}", System.currentTimeMillis());
+        log.info("****注册中心服务端启动****", System.currentTimeMillis());
     }
 
 }
