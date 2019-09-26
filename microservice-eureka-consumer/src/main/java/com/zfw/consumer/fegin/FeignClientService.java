03ae59e5c8bda3304ec04d18894a702ea93db81b
zhengliangs <zhengliang166@126.com>
2019-09-26 13:41:56 +0800

    放开ribbon

--- microservice-eureka-consumer/src/main/java/com/zfw/consumer/fegin/FeignClientService.java
+++ microservice-eureka-consumer/src/main/java/com/zfw/consumer/fegin/FeignClientService.java
@@ -1,6 +1,6 @@
 package com.zfw.consumer.fegin;
 
-import com.zfw.consumer.Hystrix.FeignHystrix;
+import com.zfw.consumer.hystrix.FeignHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
