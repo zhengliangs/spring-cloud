7bf2a019a747e1e88d3f896ad420e21507fe0f94
zhengliangs <zhengliang166@126.com>
2019-12-20 17:31:25 +0800

    服务监控admin

---
+++ microservice-admin/src/main/java/com/example/microserviceadmin/MicroserviceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAdminApplication.class, args);
+    }
+
+}
