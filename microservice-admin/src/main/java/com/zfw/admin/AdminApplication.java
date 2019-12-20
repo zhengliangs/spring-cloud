4f7586ae6f5c2201fcb9634b83477523b117961d
zhengliangs <zhengliang166@126.com>
2019-12-20 17:37:58 +0800

    修改

--- microservice-admin/src/main/java/com/zfw/admin/AdminApplication.java
+++ microservice-admin/src/main/java/com/zfw/admin/AdminApplication.java
@@ -1,13 +1,13 @@
-package com.example.microserviceadmin;
+package com.zfw.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceAdminApplication {
+public class AdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceAdminApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 
 }
--- microservice-admin/src/test/java/com/zfw/admin/MicroserviceAdminApplicationTests.java
+++ microservice-admin/src/test/java/com/zfw/admin/MicroserviceAdminApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.microserviceadmin;
+package com.zfw.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
