3675124313a5da8db7307645745ea436960e0e72
zhengll <zhengliang166@126.com>
2019-12-22 22:50:16 +0800

    admin实现

--- microservice-admin/src/main/java/com/zfw/admin/AdminApplication.java
+++ microservice-admin/src/main/java/com/zfw/admin/AdminApplication.java
@@ -1,9 +1,13 @@
 package com.zfw.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
 public class AdminApplication {
 
     public static void main(String[] args) {
