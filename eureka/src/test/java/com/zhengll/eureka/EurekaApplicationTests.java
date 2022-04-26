802163af59f2db49a35a031a8d5e99ca6d3279da
船长 <zhengliang166@126.com>
2022-04-26 11:43:28 +0800

    整理eureka包层级和yml配置文件

--- eureka/src/test/java/com/zhengll/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/zhengll/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.zhengll.eurekaserver;
+package com.zhengll.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class EurekaApplicationTests {
 
     @Test
     public void contextLoads() {
